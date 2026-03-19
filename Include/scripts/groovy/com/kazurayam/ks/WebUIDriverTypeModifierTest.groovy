59970d7c140fe497d07086ae949a46c3fb297e20
kazurayam <kazuaki.urayama@gmail.com>
2026-03-19 21:02:41 +0900

    #3

--- Include/scripts/groovy/com/kazurayam/ks/WebUIDriverTypeModifierTest.groovy
+++ Include/scripts/groovy/com/kazurayam/ks/WebUIDriverTypeModifierTest.groovy
@@ -3,6 +3,7 @@ package com.kazurayam.ks
 import static org.junit.Assert.assertNotNull
 import static org.junit.Assert.assertNull
 import static org.junit.Assert.assertTrue
+import static org.junit.Assert.assertFalse
 
 import org.junit.BeforeClass
 import org.junit.Test
@@ -30,14 +31,28 @@ public class WebUIDriverTypeModifierTest {
 	}
 	
 	@Test
+	public void test_isDefinedDriverName_undefined() {
+		assertFalse(WebUIDriverType.isDefinedDriverName("undefined"))
+	}
+	
+	@Test
 	public void test_valueOfByDriverName_ChromeHeadless() {
 		assertNotNull(WebUIDriverType.valueOfByDriverName("Chrome (headless)"))
 	}
 	
 	@Test
-	public void test_valueOfByDriverName_unknown() {
-		assertNull(WebUIDriverType.valueOfByDriverName("unknown"))
+	public void test_valueOfByDriverName_undefined() {
+		assertNull(WebUIDriverType.valueOfByDriverName("undefined"))
 	}
 	
-	
+	@Test
+	public void test_getDriverNames() {
+		List<String> driverNames = WebUIDriverType.getDriverNames()
+		println driverNames
+		assertTrue(driverNames.contains("Chrome"))
+		assertTrue(driverNames.contains("Chrome (headless)"))
+		assertTrue(driverNames.contains("Firefox"))
+		assertTrue(driverNames.contains("Firefox (headless)"))
+		assertTrue(driverNames.contains("Edge Chromium"))
+	}
 }
--- Keywords/com/kazurayam/ks/WebUIDriverTypeModifier.groovy
+++ Keywords/com/kazurayam/ks/WebUIDriverTypeModifier.groovy
@@ -7,6 +7,7 @@ import com.kms.katalon.core.webui.driver.WebUIDriverType
 public class WebUIDriverTypeModifier {
 
 	public static void apply() {
+		
 		WebUIDriverType.metaClass.static.valueOfByDriverName = { driverName ->
 			List<WebUIDriverType> list = WebUIDriverType.values() as List
 			List<WebUIDriverType> filtered = list.stream()
@@ -25,5 +26,15 @@ public class WebUIDriverTypeModifier {
 			WebUIDriverType dt = WebUIDriverType.valueOfByDriverName(driverName)
 			return (dt != null)
 		}
+		
+		WebUIDriverType.metaClass.static.getDriverNames = { ->
+			List<WebUIDriverType> list = WebUIDriverType.values() as List
+			List<String> sorted = list.stream()
+				.map({ dt -> dt.getDriverName() })
+				.sorted()
+				.collect(Collectors.toList())
+			return sorted
+		}
+		
 	}
 }
